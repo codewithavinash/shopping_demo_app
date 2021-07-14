9be82e9185480e50d761141f7ac32d4df35b2f83
codewithavinash <avinashthakur.edu@gmail.com>
2021-07-14 15:29:40 +0530

    updating widget with providers in stateless widget

--- lib/provider/products.dart
+++ lib/provider/products.dart
@@ -38,12 +38,27 @@ class Products with ChangeNotifier {
     ),
   ];
 
+  var _showFavouritesOnly = false;
+
   List<Product> get items {
+    if (_showFavouritesOnly) {
+      return _items.where((prodItem) => prodItem.isFavorite).toList();
+    }
     return [..._items];
   }
 
   Product findById(String id) {
     return _items.firstWhere((prod) => prod.id == id);
+  }
+
+  void showFavoritesOnly() {
+    _showFavouritesOnly = true;
+    notifyListeners();
+  }
+
+  void showAll() {
+    _showFavouritesOnly = false;
+    notifyListeners();
   }
 
   void addProduct() {
--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../provider/products.dart';
 import '../widgets/products_grid.dart';
+
+enum FilterOptions {
+  Favorites,
+  All,
+}
 
 class ProductsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final productsContainer = Provider.of<Products>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              // print(selectedValue);
+              if (selectedValue == FilterOptions.Favorites) {
+                productsContainer.showFavoritesOnly();
+              } else {
+                productsContainer.showAll();
+              }
+            },
+            icon: Icon(
+              Icons.more_vert,
+            ),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Only Favourites'),
+                value: FilterOptions.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text('Show all Fouvarites'),
+                value: FilterOptions.All,
+              ),
+            ],
+          ),
+        ],
       ),
       body: new ProductsGrid(),
     );
