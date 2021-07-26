38912b421b7bb8a253de6f42e07d8478285f7c85
codewithavinash <avinashthakur.edu@gmail.com>
2021-07-27 01:57:22 +0530

    App Drawer : Order widget fixed!

--- lib/provider/orders.dart
+++ lib/provider/orders.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
-import '../provider/cart.dart';
+
+import './cart.dart';
 
 class OrderItem {
   final String id;
@@ -10,25 +11,27 @@ class OrderItem {
   OrderItem({
     @required this.id,
     @required this.amount,
-    @required this.dateTime,
     @required this.products,
+    @required this.dateTime,
   });
 }
 
 class Orders with ChangeNotifier {
   List<OrderItem> _orders = [];
+
   List<OrderItem> get orders {
-    return [...orders];
+    return [..._orders];
   }
 
   void addOrder(List<CartItem> cartProducts, double total) {
     _orders.insert(
       0,
       OrderItem(
-          id: DateTime.now().toString(),
-          amount: total,
-          dateTime: DateTime.now(),
-          products: cartProducts),
+        id: DateTime.now().toString(),
+        amount: total,
+        dateTime: DateTime.now(),
+        products: cartProducts,
+      ),
     );
     notifyListeners();
   }
--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import '../widgets/app_drawer.dart';
 import 'package:provider/provider.dart';
 
 import '../provider/orders.dart' show Orders;
 import '../widgets/order_item.dart';
+import '../widgets/app_drawer.dart';
 
 class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
+
   @override
   Widget build(BuildContext context) {
     final orderData = Provider.of<Orders>(context);
@@ -16,8 +17,9 @@ class OrdersScreen extends StatelessWidget {
       ),
       drawer: AppDrawer(),
       body: ListView.builder(
-          itemCount: orderData.orders.length,
-          itemBuilder: (ctx, i) => OrderItem(orderData.orders[i])),
+        itemCount: orderData.orders.length,
+        itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+      ),
     );
   }
 }
