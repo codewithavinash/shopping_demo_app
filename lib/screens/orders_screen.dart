7a7c2f489bddad8120e1507d1e09b373ec13c1df
codewithavinash <avinashthakur.edu@gmail.com>
2021-08-16 17:18:30 +0530

    Order Expand Error!

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/retry.dart';
 import 'package:provider/provider.dart';
 
 import '../provider/orders.dart' show Orders;
@@ -16,34 +17,61 @@ class _OrdersScreenState extends State<OrdersScreen> {
   var _isLoading = false;
   @override
   void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    super.initState();
+    // Future.delayed(Duration.zero).then((_) async {
+    _isLoading = true;
+    // Provider.of<Orders>(context, listen: false)
+    //     .fetchAndSetOrders()
+    //     .then((_) {
+
+    //     });
+    // setState(() {
+    //   _isLoading = false;
+    //   // });
+    // });
+    // super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    print('Your Order Rebuilds');
+    // final orderData = Provider.of<Orders>(context); // this will result in loop
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Your Orders'),
-      ),
-      drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: orderData.orders.length,
-              itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
-            ),
-    );
+        appBar: AppBar(
+          title: Text('Your Orders'),
+        ),
+        drawer: AppDrawer(),
+        body: FutureBuilder(
+            future:
+                Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+            builder: (ctx, dataSnapshot) {
+              if (dataSnapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else {
+                //error case
+                if (dataSnapshot.error != null) {
+                  //error handling
+                  return Center(child: Text('Error'));
+                } else {
+                  //success case
+                  return Consumer<Orders>(
+                    builder: (ctx, orderData, child) => ListView.builder(
+                      itemCount: orderData.orders.length,
+                      itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+                    ),
+                  );
+                }
+              }
+            })
+        // _isLoading
+        //     ? Center(
+        //         child: CircularProgressIndicator(),
+        //       )
+        //     : ListView.builder(
+        //         itemCount: orderData.orders.length,
+        //         itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+        //       ),
+        );
   }
 }
